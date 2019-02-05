7ca6875265aadbf20c6919ad0211f5d6d6765787
juless89 <j.m.r.kramer@gmail.com>
2019-02-05 16:13:25 +0100

    STEEM/SBD Ticker

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, TickerData
 
 urlpatterns = [
     path('', HomePageView.as_view(), kwargs={'account': 'steempytutorials'}, name='home'),
-    path('@<slug:account>/', HomePageView.as_view(), name='home')
+    path('api/ticker/data/', TickerData.as_view()),
+    path('@<slug:account>/', HomePageView.as_view(), name='home'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views.generic import ListView
 from django.http import HttpResponseNotFound
 from .services import account_history
 from beem.exceptions import AccountDoesNotExistsException
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+import requests, json
 
 # View for the homepage that will display blog posts of the author
 # in a list.
@@ -23,3 +26,29 @@ class HomePageView(ListView):
 
         # Use the original function to generate the html reponse.
         return super().get(request, *args, **kwargs)
+
+
+# APIview for coinmarketcap STEEM and SBD ticker prices.
+class TickerData(APIView):
+    # Unused user authentication classes
+    authentication_classes = []
+    permission_classes = []
+
+    # Get request for coinmarketcap, returns a string of the current price
+    def get_market_price(self, token):
+        api_url = 'https://api.coinmarketcap.com/v1/ticker/{}/'.format(token)
+        response = requests.get(api_url)
+    
+        if response.status_code == 200:
+            return json.loads(response.content.decode('utf-8'))[0]['price_usd']
+        else:
+            return None
+    
+    # Retrieve STEEM and SBD market prices, return a dict
+    def get(self, request, format=None):
+        data = {
+            "STEEM": '{:.3f}'.format(float(self.get_market_price('steem'))),
+            "SBD": '{:.3f}'.format(float(self.get_market_price('steem-dollars'))),
+        }
+
+        return Response(data)
